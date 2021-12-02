c82f2cb827693231fe42c00ea3dab9febe868f76
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-02 01:37:16 -0300

    :lipstick: style: Added styles for the username and image

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -8,11 +8,35 @@ export const HeaderContainer = styled.header`
   background: var(--black-900);
   margin: 0 auto;
   padding: 1rem 0;
+  box-shadow: 0 0 0.25rem 0.15rem var(--black-900);
 
   p {
     font-size: 1.25rem;
     font-weight: 700;
     color: var(--white-900);
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .user-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-left: 1rem;
+      cursor: pointer;
+
+      img {
+        clip-path: circle();
+      }
+
+      strong {
+        color: var(--white-900);
+        margin-left: 0.75rem;
+      }
+    }
   }
 
   button {
