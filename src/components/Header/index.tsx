cace61d342ee6a774ba971b0feec1d4b085b45ac
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-02 01:36:45 -0300

    :sparkles: feat: Added username and image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,29 @@
 import { HeaderContainer } from './style'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import VercelIcon from '/public/vercel.svg'
+import { FaUserCircle } from 'react-icons/fa'
 
 const Header = () => {
+  const userLogged = false
+
   return (
     <HeaderContainer>
       <p>AnunX</p>
-      <button>Anunciar e Vender</button>
+      <div>
+        <Link href='/user/publish' passHref>
+          <button>Anunciar e Vender</button>
+        </Link>
+        <div className='user-container'>
+          {userLogged ? (
+            <Image src={VercelIcon} alt='User picture' width={100} />
+          ) : (
+            <FaUserCircle color='white' size={32} />
+          )}
+          <strong>Gabriel Bittencourt</strong>
+        </div>
+      </div>
     </HeaderContainer>
   )
 }
