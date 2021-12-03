6e8d2c172c2d1106d19cd989e2420581a93d4bc2
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-03 19:55:30 -0300

    :art: feat: Improved the new code for the header menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,55 @@
-import { HeaderContainer } from './style'
+import { useState, useRef } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 
-import VercelIcon from '/public/vercel.svg'
+import { HeaderContainer } from './style'
 import { FaUserCircle } from 'react-icons/fa'
+import { useClickOutside } from '../../hooks/useClickOutside'
 
 const Header = () => {
-  const userLogged = false
+  const [openMenu, setOpenMenu] = useState(false)
+  const ref = useRef(null)
+
+  const handleOpenMenu = () => setOpenMenu(!openMenu)
+
+  const handleClickOutside = () => {
+    handleOpenMenu()
+  }
+
+  useClickOutside(ref, handleClickOutside)
 
   return (
     <HeaderContainer>
-      <p>AnunX</p>
-      <div>
-        <Link href='/user/publish' passHref>
-          <button>Anunciar e Vender</button>
+      <nav className='header-nav'>
+        <Link href='/' passHref>
+          <p>AnunX</p>
         </Link>
-        <div className='user-container'>
-          {userLogged ? (
-            <Image src={VercelIcon} alt='User picture' width={100} />
-          ) : (
-            <FaUserCircle color='white' size={32} />
-          )}
-          <strong>Gabriel Bittencourt</strong>
+        <div>
+          <Link href='/user/publish' passHref>
+            <button>Anunciar e Vender</button>
+          </Link>
+          <div className='user-container' onClick={handleOpenMenu}>
+            <FaUserCircle color='white' size={28} />
+            <strong>Gabriel Bittencourt</strong>
+            {openMenu ? (
+              <>
+                <span className='arrow'></span>
+                <nav ref={ref} className='menu-links'>
+                  <Link href='/user/dashboard'>
+                    <a>Meus anúncios</a>
+                  </Link>
+                  <Link href='/user/publish'>
+                    <a>Publicar novo anúncio</a>
+                  </Link>
+                  <hr />
+                  <Link href='#'>
+                    <a>Sair</a>
+                  </Link>
+                </nav>
+              </>
+            ) : null}
+          </div>
         </div>
-      </div>
+      </nav>
     </HeaderContainer>
   )
 }
