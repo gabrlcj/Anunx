835bb97ed711b9a22876507ca19ba3a9744d2999
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-03 19:57:37 -0300

    :zap: chore: Added Card component and improved code

--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -1,12 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { Container } from '../../../styles/container'
-import {
-  DashboardSection,
-  ProductsSection,
-  ProductCard,
-} from './dashboardStyle'
+import { DashboardSection, ProductsSection } from './dashboardStyle'
+import Card from '../../components/Card'
 
 import VercelIcon from '/public/vercel.svg'
 
@@ -21,50 +17,50 @@ const Dashboard: NextPage = () => {
         <h1>Meus Anúncios</h1>
         <button>Publicar novo anúncio</button>
         <ProductsSection>
-          <ProductCard>
-            <Image src={VercelIcon} alt='Produto' />
-            <div className='product-details'>
-              <h2>Produto X</h2>
-              <p>R$ 60,00</p>
-            </div>
-            <div className='button-container'>
-              <button>Editar</button>
-              <button>Remover</button>
-            </div>
-          </ProductCard>
-          <ProductCard>
-            <Image src={VercelIcon} alt='Produto' />
-            <div className='product-details'>
-              <h2>Produto X</h2>
-              <p>R$ 60,00</p>
-            </div>
-            <div className='button-container'>
-              <button>Editar</button>
-              <button>Remover</button>
-            </div>
-          </ProductCard>
-          <ProductCard>
-            <Image src={VercelIcon} alt='Produto' />
-            <div className='product-details'>
-              <h2>Produto X</h2>
-              <p>R$ 60,00</p>
-            </div>
-            <div className='button-container'>
-              <button>Editar</button>
-              <button>Remover</button>
-            </div>
-          </ProductCard>
-          <ProductCard>
-            <Image src={VercelIcon} alt='Produto' />
-            <div className='product-details'>
-              <h2>Produto X</h2>
-              <p>R$ 60,00</p>
-            </div>
-            <div className='button-container'>
-              <button>Editar</button>
-              <button>Remover</button>
-            </div>
-          </ProductCard>
+          <Card
+            image={VercelIcon}
+            title='Produto X'
+            subtitle='R$ 60,00'
+            actions={
+              <>
+                <button>Editar</button>
+                <button>Excluir</button>
+              </>
+            }
+          />
+          <Card
+            image={VercelIcon}
+            title='Produto X'
+            subtitle='R$ 60,00'
+            actions={
+              <>
+                <button>Editar</button>
+                <button>Excluir</button>
+              </>
+            }
+          />
+          <Card
+            image={VercelIcon}
+            title='Produto X'
+            subtitle='R$ 60,00'
+            actions={
+              <>
+                <button>Editar</button>
+                <button>Excluir</button>
+              </>
+            }
+          />
+          <Card
+            image={VercelIcon}
+            title='Produto X'
+            subtitle='R$ 60,00'
+            actions={
+              <>
+                <button>Editar</button>
+                <button>Excluir</button>
+              </>
+            }
+          />
         </ProductsSection>
       </DashboardSection>
     </Container>
