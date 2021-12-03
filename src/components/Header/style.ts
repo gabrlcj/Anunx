b372452f5e588c20ca403511078ee2f8bc3ac7dc
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-03 19:55:59 -0300

    :lipstick: style: Fixed and improved the styles for the header

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,57 +1,108 @@
 import styled from 'styled-components'
+import { FadeIn } from '../../../styles/animations'
 
 export const HeaderContainer = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-
   background: var(--black-900);
-  margin: 0 auto;
   padding: 1rem 0;
   box-shadow: 0 0 0.25rem 0.15rem var(--black-900);
 
-  p {
-    font-size: 1.25rem;
-    font-weight: 700;
-    color: var(--white-900);
-  }
-
-  div {
+  .header-nav {
+    max-width: 1240px;
+    margin: 0 auto;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
 
-    .user-container {
+    p {
+      font-size: 1.25rem;
+      font-weight: 700;
+      color: var(--white-900);
+      cursor: pointer;
+    }
+
+    div {
       display: flex;
       align-items: center;
       justify-content: center;
-      margin-left: 1rem;
-      cursor: pointer;
 
-      img {
-        clip-path: circle();
-      }
+      .user-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: 2rem;
+        cursor: pointer;
+        position: relative;
 
-      strong {
-        color: var(--white-900);
-        margin-left: 0.75rem;
+        img {
+          clip-path: circle();
+        }
+
+        strong {
+          color: var(--white-900);
+          margin-left: 0.75rem;
+        }
+
+        span {
+          animation: ${FadeIn} 0.4s ease;
+        }
+
+        .menu-links {
+          background: var(--white-900);
+          border-radius: 0.25rem;
+
+          display: flex;
+          align-items: flex-start;
+          justify-content: center;
+          flex-direction: column;
+
+          position: absolute;
+          bottom: -500%;
+          right: -30%;
+          animation: ${FadeIn} 0.3s ease;
+
+          a {
+            color: var(--black-900);
+            width: 100%;
+            padding: 0.75rem;
+            border-radius: 0.25rem;
+
+            &:hover {
+              background: var(--grey-100);
+            }
+          }
+
+          hr {
+            background: var(--black-900);
+            width: 100%;
+          }
+        }
+
+        .arrow {
+          position: absolute;
+          bottom: -115%;
+          width: 1.125rem;
+          height: 1.125rem;
+          display: block;
+          background: var(--white-900);
+          transform: translateY(-50%) rotate(45deg);
+        }
       }
     }
-  }
 
-  button {
-    font-size: 0.875rem;
-    background: transparent;
-    border: 1px solid var(--grey-900);
-    border-radius: 0.25rem;
-    color: var(--white-900);
-    padding: 0.5rem 0.75rem;
-    text-transform: uppercase;
+    button {
+      font-size: 0.875rem;
+      background: transparent;
+      border: 1px solid var(--grey-900);
+      border-radius: 0.25rem;
+      color: var(--white-900);
+      padding: 0.5rem 0.75rem;
+      text-transform: uppercase;
 
-    transition: border 0.2s ease;
+      transition: border 0.2s ease;
 
-    &:hover {
-      border: 1px solid var(--white-900);
+      &:hover {
+        border: 1px solid var(--white-900);
+      }
     }
   }
 `
