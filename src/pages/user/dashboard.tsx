469d680b4f33e32d09696155129dab48ef8d4f79
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-07 14:40:58 -0300

    :art: fix: Changed the images for the cards

--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -4,8 +4,6 @@ import { Container } from '../../../styles/container'
 import { DashboardSection, ProductsSection } from './dashboardStyle'
 import Card from '../../components/Card'
 
-import VercelIcon from '/public/vercel.svg'
-
 const Dashboard: NextPage = () => {
   return (
     <Container>
@@ -18,7 +16,9 @@ const Dashboard: NextPage = () => {
         <button>Publicar novo anúncio</button>
         <ProductsSection>
           <Card
-            image={VercelIcon}
+            image={
+              'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            }
             title='Produto X'
             subtitle='R$ 60,00'
             actions={
@@ -29,7 +29,9 @@ const Dashboard: NextPage = () => {
             }
           />
           <Card
-            image={VercelIcon}
+            image={
+              'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            }
             title='Produto X'
             subtitle='R$ 60,00'
             actions={
@@ -40,7 +42,9 @@ const Dashboard: NextPage = () => {
             }
           />
           <Card
-            image={VercelIcon}
+            image={
+              'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            }
             title='Produto X'
             subtitle='R$ 60,00'
             actions={
@@ -51,7 +55,9 @@ const Dashboard: NextPage = () => {
             }
           />
           <Card
-            image={VercelIcon}
+            image={
+              'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+            }
             title='Produto X'
             subtitle='R$ 60,00'
             actions={
