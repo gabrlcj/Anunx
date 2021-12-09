2b6c555256a2f608f89ae7ff8f590d0d5a987fc0
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-09 19:23:58 -0300

    :sparkles: chore: Added the toast container for the toast notification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
-import { GlobalStyles } from '../../styles/global'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
+import { GlobalStyles } from '../../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <ToastContainer autoClose={3000} position='top-right' />
       <Header />
       <Component {...pageProps} />
       <Footer />
