81479b3b6d7793ba10915164e09d9a3262d928a4
Gabriel <gabrielbittencourt57@gmail.com>
2021-12-09 19:21:31 -0300

    :sparkles: feat: Created signin page

---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { Container } from '../../../../styles/container'
+
+const Signin: NextPage = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Bem vindo | AnunX</title>
+      </Head>
+    </Container>
+  )
+}
+
+export default Signin
